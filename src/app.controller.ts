2dbc2a979c5b4d53f2d1cf646b2ee3cbe063e0b8
David <leffiedj@gmail.com>
2022-01-20 15:53:41 +0000

    First commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('post')
+  receiveJson(@Body() b){
+    return this.appService.echoBody(b);
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+
+
+@Injectable()
+export class AppService {
+  static globalVar: any;
+  getHello(): string {
+    return AppService.globalVar;
+  }
+
+  echoBody(b){
+    AppService.globalVar =  b;
+  }
+}
